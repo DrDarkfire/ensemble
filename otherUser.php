abf9f858336ddfb6f8e46ecc9955690776e61f64
Mai Trinh <46837255+mai-trinh@users.noreply.github.com>
2020-04-22 21:00:06 -0400

    Follow/Unfollow functionality
    profile.php allows user to view who they are following and their followers, from there user can unfollow (unfollow_f.php). otherUser.php and follow_f.php are old functionalities with some modifications in this version.

--- otherUser.php
+++ otherUser.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $_SESSION['uid'] = 4;
-$_SESSION['fid'] = 2;
+$_SESSION['fid'] = 3;
 ?>
 
 <!DOCTYPE html>
@@ -22,10 +22,10 @@ $_SESSION['fid'] = 2;
             <!-- Drop down menu of user options - from w3 schools  -->
             <div>
               <div class="menu-icon dropdown">
-                <i class="fa fa-bars" style = "color:#FFD700"></i>
+                <i class="fa fa-bars" style ="color:#FFD700"></i>
                 <div class="dropdown-content">
                   <a href="user.html" >What's new</a>
-                  <a href="#" >My Ensemble</a>
+                  <a href="profile.php" >My Ensemble</a>
                   <a href="myMusic.php" >My Music</a>
                   <a href="updateProfile.html" >Edit Profile</a>
                 </div>
@@ -36,8 +36,7 @@ $_SESSION['fid'] = 2;
 	include_once("db_connect.php");
 
 	$fid = $_SESSION['fid'];
-	//$fid = $_SESSION['fid'];
-	$qStr = "SELECT * FROM user WHERE uid=$fid;";
+	$qStr = "SELECT DISTINCT username FROM user WHERE uid=$fid;";
 	$qRes = $db->query($qStr);
 	
 	if ($qRes != FALSE) {
@@ -51,8 +50,9 @@ $_SESSION['fid'] = 2;
           <div class = "other-profile">
             <img class = "other-profile-pic" src = "https://pbs.twimg.com/profile_images/1155645244563742721/tuCu6BT-_400x400.jpg">
             <p class = "other-profile-name"> <?php echo $username; ?> </p>
-            <button class ="follow-button" id="follow_btn" onclick="window.location.href='follow_f.php'"> Follow </button>
+	    <input type='button' class ='follow-button' id='follow_btn' value = 'Follow' onclick='window.location.href='follow_f.php''>
           </div>
+
           <hr style = "border: 2px solid black; width: 90%">
           <div class = "music-grid">
             <div class = "music-container">
@@ -92,17 +92,15 @@ $_SESSION['fid'] = 2;
 		$query = "SELECT * FROM follow where fid=$fid;";
 		$result = $db->query($query);
 		if ($result != FALSE) {
-		while ($row = $result->fetch()) {
-			$uid = $row['uid'];
-			$pStr = "SELECT * FROM user WHERE uid=$uid;";
-			$pRes = $db->query($pStr);
-			$row = $pRes->fetch();
-			$follower = $row['username'];
-
-
-			$str = "<TR><TD><img class = 'table-pro-pic' src = 'https://pbs.twimg.com/profile_images/1155645244563742721/tuCu6BT-_400x400.jpg'/><TD><TD>$follower</TD></TR>\n";
-			print $str;
-			}
+			while ($row = $result->fetch()) {
+				$uid = $row['uid'];
+				$pStr = "SELECT * FROM user WHERE uid=$uid;";
+				$pRes = $db->query($pStr);
+				$row = $pRes->fetch();
+				$follower = $row['username'];
+				$str = "<TR><TD><img class = 'table-pro-pic' src = 'https://pbs.twimg.com/profile_images/1155645244563742721/tuCu6BT-_400x400.jpg'/></TD><TD>$follower</TD></TR>\n";
+				print $str;
+				}
 		}
 		?>
       
@@ -111,7 +109,7 @@ $_SESSION['fid'] = 2;
             </div>
           <div class = "music-container">
             <center>
-              <h2> Playlists Fake Name has made </h2>
+              <h2> Playlists <?php echo $username; ?> has made </h2>
             <table class = "music-table" cellspacing ="5" cellpadding="10">
               <TR><TD>Name</TD></TR>
               <TR><TD>Name</TD></TR>
@@ -124,7 +122,7 @@ $_SESSION['fid'] = 2;
           </div>
           <div class = "music-container">
             <center>
-              <h2> Fake Name's most listned to songs </h2>
+              <h2> <?php echo $username; ?>'s most listned to songs </h2>
             <table class = "music-table" cellspacing ="5" cellpadding="10">
               <TR><TD>Name</TD></TR>
               <TR><TD>Name</TD></TR>
---
+++ profile.php
@@ -0,0 +1,146 @@
+<?php 
+session_start();
+$_SESSION['uid'] = 4;
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8">
+        <meta name = "Sample Site">
+        <title>Ensemble</title>
+        <link rel="stylesheet" type="text/css" href="ensemble.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+      <!-- Top bar of the webpage, contains logo and drop down menu with links -->
+      <div class = "topBar">
+          <a href = "index.html">
+          <img src="logo.jpeg" class = "icon"/> </a>
+          <text class = "barText">Ensemble</text>
+          <!-- Drop down menu of user options - from w3 schools  -->
+          <div>
+            <div class="menu-icon dropdown">
+              <i class="fa fa-bars" style = "color:#FFD700"></i>
+              <div class="dropdown-content">
+                <a href="user.html" >What's new</a>
+                <a href="#" >My Ensemble</a>
+                <a href="#" >My Music</a>
+                <a href="updateProfile.php" >Edit Profile</a>
+              </div>
+            </div>
+         </div>
+      </div>
+
+        <!-- Main portion of this page containing user information -->
+	<?php
+	  include_once("db_connect.php");
+	  $uid = $_SESSION['uid'];
+
+	  $query = "SELECT * FROM user WHERE uid=$uid;";
+	  $qRes = $db->query($query);
+
+	  if ($qRes != FALSE) {
+		$row = $qRes->fetch();
+		$username = $row['username'];
+		$email = $row['email'];
+		$bdate = $row['bdate'];
+	  }		
+	?> 
+        <div class = "input">
+            <center>
+               <div class = "form-container">
+                 <h1 style = "color:#333"> <?php echo "Welcome back, ".$username." !" ?> </h1>
+                </div>
+            </center>
+        </div>
+        <!-- Showing user information  -->
+	    <div class = "music-container">
+		<h2> Your Account Information </h2>
+		      <table class = "music-table" cellspacing ="5" cellpadding="10">
+			<TR><TD>Your Username:</TD><TD><?php echo $username?></TD></TR>
+			<TR><TD>Your Email:</TD><TD><?php echo $email?></TD></TR>
+			<TR><TD>Your Birthday</TD><TD><?php echo $bdate?></TD></TR>
+		      </table>
+	    </div>
+
+        <!-- Showing users whom user follows -->
+	    <div class = "music-container">
+	      <h2> Following </h2>
+
+	      <table class = "music-table" cellspacing ="5" cellpadding="10">
+	      <!-- retrieve accs user are following-->
+	      <?php
+		$qStr = "SELECT * FROM user WHERE uid IN (SELECT fid FROM follow WHERE uid = $uid);";
+		$qRes = $db->query($qStr);
+		if ($qRes != FALSE) 
+		{
+		  while ($row = $qRes->fetch()) 
+		  {
+		    $username = $row['username'];
+		    $follow_id = $row['uid'];
+		    $str = "<TR><TD>$username</TD><TD><button id='unfollowBtn' onclick='unfollowUser($follow_id);'>Unfollow</button></TD>
+<TD><button id='viewProfBtn' onclick='viewProfile($follow_id);'>View Profile</button></TD></TR>";
+		    print($str);
+		  }
+		}
+	      ?>
+	      </table>
+
+	    <!-- hidden form -->
+            <form name="unfollow_form" id="unfollow_form" method ="POST" action="unfollow_f.php">
+		  <p id='unfollow_ms' style="display:none;" >Unfollow user ?</p>
+                  <input type = 'text' name = 'unfollow_id' id ='unfollow_id' value = 'text' style='display:none;'>
+		  <input class ="submit-button main-button" id='unfollow_submit' style="display:none;" type = "submit" value = "Unfollow" >
+            </form>
+	    </div>
+
+	    <div class = "music-container">
+		<h2>  Followers </h2>
+		      <table class = "music-table" cellspacing ="5" cellpadding="10">
+			<?php
+			$qStr = "SELECT * FROM user WHERE uid IN (SELECT uid FROM follow WHERE fid = $uid);";
+			$qRes = $db->query($qStr);
+
+			if ($qRes != FALSE) {
+				while ($row = $qRes->fetch()) {
+					$username = $row['username'];
+					$str = "<TR><TD>$username</TD></TR>";
+					print($str);
+				}
+			}
+		       ?>
+		      </table>
+	    </div>
+
+	   <div class = "music-container">
+		<h2> Your playlists</h2>
+		<table class = "music-table" cellspacing ="5" cellpadding="10">
+			<TR><TD>Playlist 1</TD></TR>
+			<TR><TD>Playlist 2</TD></TR>
+		</table>
+	   </div>
+	    
+
+
+        <!-- footer -->
+        <div class = "footer">
+            <center>
+                <text class = "copyright"> &copy; BMC inc. | 2020 </text>
+            </center>
+        </div>
+    </body>
+
+    <script>
+    function unfollowUser(id) {
+	    console.log(id);
+	    document.getElementById("unfollow_id").value = id;
+	    document.getElementById("unfollow_ms").style.display = 'block';
+	    document.getElementById("unfollow_submit").style.display = 'block';
+    }
+
+    function viewProfile(id) {
+	    
+    }
+    </script>
+</html>
